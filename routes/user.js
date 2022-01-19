af9d1c2220f065355c6aff0cf52eec99da3eff69
Van Nguyen <npnv.vn1@gmail.com>
2022-01-19 19:41:00 +0700

    upload

--- routes/user.js
+++ routes/user.js
@@ -14,6 +14,19 @@ const {
 
 const router = express.Router();
 
+router.get('/:id/avatar', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user || !user.avatar) throw new Error();
+
+    res.set('Content-type', 'image/png');
+    res.status(200).send(user.avatar);
+  } catch (error) {
+    res.status(400).json();
+  }
+});
+
 router.use(auth);
 
 router.get('/me', getMe);
@@ -63,19 +76,6 @@ router.delete('/me/avatar', auth, async (req, res) => {
   req.user.avatar = undefined;
   await req.user.save();
   res.status(200).json();
-});
-
-router.get('/:id/avatar', async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-
-    if (!user || !user.avatar) throw new Error();
-
-    res.set('Content-type', 'image/png');
-    res.status(200).send(user.avatar);
-  } catch (error) {
-    res.status(400).json();
-  }
 });
 
 module.exports = router;
